1103f89cf50c0a47c484ddf8548e42ac8d31dc76
Liran BG <liranbg@gmail.com>
2022-09-07 09:04:26 +0300

    with fixes, Is and CI

--- errors.go
+++ errors.go
@@ -1,3 +1,18 @@
+// Copyright 2019 Iguazio
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 // Package errors provides an api similar to github.com/nuclio/nuclio/pkg/errors
 // However we don't carry stack trace around for performance
 // (see https://github.com/pkg/errors/issues/124)
@@ -290,7 +305,7 @@ func (err *Error) Format(s fmt.State, verb rune) {
 		}
 		fallthrough
 	case 's':
-		fmt.Fprintf(s, err.Error()) // nolint: errcheck
+		fmt.Fprint(s, err.Error()) // nolint: errcheck
 	case 'q':
 		fmt.Fprintf(s, "%q", err.Error()) // nolint: errcheck
 	}
